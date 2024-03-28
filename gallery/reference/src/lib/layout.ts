e5f6cf5e0b012773badb0e60e24e99ec39af5aaa
Ashley Davis <ashley@codecapers.com.au>
2024-03-28 18:34:56 +1000

    Refactoring to be more functional.

--- gallery/reference/src/lib/layout.ts
+++ gallery/reference/src/lib/layout.ts
@@ -153,55 +153,53 @@ export function computePartialLayout(existingLayout: IGalleryLayout | undefined,
     // For all rows, except the last row, stretch the items towards the right hand boundary.
     //
     for (let rowIndex = startingRowIndex; rowIndex < rows.length-1; rowIndex++) {
-        const row = rows[rowIndex];
         const nextRow = rows[rowIndex+1];
-        if (!headingsMatch(row.headings, nextRow.headings)) {
+        if (!headingsMatch(rows[rowIndex].headings, nextRow.headings)) {
             continue; // Don't expand the last row in each group.
         }
 
-        const gap = galleryWidth - row.width;
-        const deltaWidth = gap / row.items.length;
+        const gap = galleryWidth - rows[rowIndex].width;
+        const deltaWidth = gap / rows[rowIndex].items.length;
 
         let maxThumbHeight = 0;
-        row.width = 0;
+        rows[rowIndex].width = 0;
 
         //
         // Expand each item to fill the gap.
         //
-        for (const item of row.items) {
+        for (const item of rows[rowIndex].items) {
             item.thumbWidth! += deltaWidth;
             item.thumbHeight = item.thumbWidth! * (1.0 / item.aspectRatio!);
-            row.width += item.thumbWidth!;
+            rows[rowIndex].width += item.thumbWidth!;
             maxThumbHeight = Math.max(maxThumbHeight, item.thumbHeight);
         }
 
-        computeFromHeight(row, maxThumbHeight);
+        rows[rowIndex] = sizeRowToHeight(rows[rowIndex], maxThumbHeight);
     }
 
     //
     // Now pull back the width of all rows so they don't overlap the right hand edge by too much.
     //
     for (let rowIndex = startingRowIndex; rowIndex < rows.length-1; rowIndex++) {
-        const row = rows[rowIndex];
         const nextRow = rows[rowIndex+1];
-        if (!headingsMatch(row.headings, nextRow.headings)) {
+        if (!headingsMatch(rows[rowIndex].headings, nextRow.headings)) {
             continue; // Don't expand the last row in each group.
         }
 
         let pullback = 1;
-        let origHeight = row.height;
+        let origHeight = rows[rowIndex].height;
         let prevHeight = origHeight;
 
         while (true) {
 
             const newHeight = origHeight - pullback;
-            computeFromHeight(row, newHeight);
+            rows[rowIndex] = sizeRowToHeight(rows[rowIndex], newHeight);
 
-            if (row.width < galleryWidth) {
+            if (rows[rowIndex].width < galleryWidth) {
                 //
                 // Pulled the row width in too far, restore the previous height.
                 //
-                computeFromHeight(row, prevHeight);
+                rows[rowIndex] = sizeRowToHeight(rows[rowIndex], prevHeight);
                 break;
             }
 
@@ -264,15 +262,21 @@ export function computePartialLayout(existingLayout: IGalleryLayout | undefined,
 }
 
 //
-// Compute thumbnail resolution from a requested height.
+// Resizes a row, computing thumbnail resolution, from a requested height.
 //
-function computeFromHeight(row: IGalleryRow, height: number): void {
-    row.height = height;
-    row.width = 0;
+function sizeRowToHeight(row: IGalleryRow, height: number): IGalleryRow {
+    const resizedItems = row.items.map(item => {
+        return {
+            ...item,
+            thumbHeight: height,
+            thumbWidth: height * item.aspectRatio,
+        };
+    });
 
-    for (const item of row.items) {
-        item.thumbHeight = height;
-        item.thumbWidth = row.height * item.aspectRatio!;
-        row.width += item.thumbWidth;
+    return {
+        ...row,
+        height,
+        width: resizedItems.reduce((acc, item) => acc + item.thumbWidth, 0),
+        items: resizedItems,
     }
 }
